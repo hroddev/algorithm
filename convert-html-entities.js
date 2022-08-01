51bcc6880a8393347deb0723b8bd7330b57b2bca
Hector Rodriguez <hector@hrod.dev>
2022-08-01 05:18:58 -0500

    more practice

---
+++ convert-html-entities.js
@@ -0,0 +1,23 @@
+/* Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities. */
+
+function convertHTML(str) {
+    const htmlEntities = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&apos;"
+    }
+    
+    return str
+        .split("")
+        .map(entity => htmlEntities[entity] || entity)
+        .join("")
+  }
+  
+  console.log(
+// convertHTML("Dolce & Gabbana")
+// convertHTML("Hamburgers < Pizza < Tacos")
+convertHTML("Sixty > twelve")
+  )
